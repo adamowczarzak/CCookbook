fc09d96f58f17b77948c30d91e77d748fd870c75
Adam <adam.owczarzak.job@gmail.com>
2023-10-30 21:47:51 +0100

    Small component setup

---
+++ CCookbook.WebApp.Components/Main/DishCard.razor.cs
@@ -0,0 +1,15 @@
+﻿namespace CCookbook.WebApp.Components.Main
+{
+    public partial class DishCard
+    {
+        public string Title { get; set; }
+
+        public int PreparationTime {get;set;}
+
+        public DishCard()
+        {
+            Title = "Spicy rice";
+            PreparationTime = 120;
+        }
+    }
+}
